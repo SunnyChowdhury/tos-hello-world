cf14e5685a563e91fdc8819b7d37f67cd5fbffae
Sunny Chowdhury <sunny@telemetrytv.com>
2026-02-25 11:13:50 -0800

    Change subtitle text in Settings component

--- src/views/Settings.tsx
+++ src/views/Settings.tsx
@@ -34,7 +34,7 @@ export function Settings() {
       <SettingsDivider />
 
       <SettingsField>
-        <SettingsLabel>Subtitle Text</SettingsLabel>
+        <SettingsLabel>Subtitle Text Test</SettingsLabel>
         <SettingsInputFrame>
           <input
             type="text"
